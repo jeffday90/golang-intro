863b7be0516dc49681c0c7dfb6d985f2c0d3f48d
Jeff Day <jeffrey.day00@gmail.com>
2020-04-15 14:26:05 -0700

    Gitting my git on.

--- hello.go
+++ hello.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hellow World")
+	fmt.Println("Before")
+	foo()
+	fmt.Println("After")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("In foo function")
+}
+
+func bar() {
+	fmt.Println("We exited control flow")
 }
